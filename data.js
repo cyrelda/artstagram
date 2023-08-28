d3b8b4629ec486c0d374fcaf45b486b640b2d064
C Victor <140266248+cyrelda@users.noreply.github.com>
2023-08-28 16:36:03 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+export const posts = [
+  {
+    name: "Pablo Picasso",
+    username: "IamPablo",
+    location: "Paris, France",
+    avatar: "images/avatar-picasso.jpg",
+    post: "images/post-picasso.jpg",
+    comment: ". . . today I realized my nose is more élégant than Dali's. Can you beat my #egoportrait @Dali_ilaD? ",
+    likes: 20112,
+  },
+  {
+    name: "Salvador Dalí",
+    username: "Dali_ilaD",
+    location: "Catalonia, Spain",
+    avatar: "images/avatar-dali.jpg",
+    post: "images/post-dali.jpg",
+    comment: "that is debatable, but regardless, I have the greater moustache @IamPablo",
+    likes: 31032,
+  },
+  {
+    name: "Henry Matisse",
+    username: "Matisse69",
+    location: "Nice, France",
+    avatar: "images/avatar-matisse.jpg",
+    post: "images/post-matisse.jpg",
+    comment:
+      "I wouldn't mind turning into a vermilion goldfish... 🐠 <br> #thoughtoftheday <br> #selfie",
+    likes: 15320,
+  },
+];
